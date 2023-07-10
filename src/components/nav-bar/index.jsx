bcf3548a6bdea81fc51258aefc7b65841cfd4b11
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2023-07-10 23:32:39 +0100

    [set up] - create new react project, set up tailwind and create nav bar component

---
+++ src/components/nav-bar/index.jsx
@@ -0,0 +1,19 @@
+const NavBar = () => {
+  return (
+    <header className="h-20 px-72 flex items-center ">
+      <ul className="flex flex-row">
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default NavBar;
